3daffde1872ec5a4f7d1a936531af89692453b7b
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-10-07 21:39:11 +0200

    Implemented Bot

---
+++ Catch-The-Led-Pattern/src/Bot.cpp
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<Arduino.h>
+
+
+
+
+#define NLED 4
+
+class Bot{
+    private:
+        int positions[NLED] = {0, 0, 0, 0};
+
+    public:
+
+        /*  builds the light pattern
+        *
+        */
+
+        void generateSequence(){
+            int count = 1;
+            srand((unsigned) time(NULL));
+            while(count <= 4){
+                int randNum = rand() % 4;
+                while(this->positions[randNum] != 0){
+                    randNum = rand() % 4;
+                }
+                this->positions[randNum] = count;
+                count++;
+            }
+        }
+
+        /*
+        *   returns the sequence
+        */
+
+        int* getSequence(){
+            return this->positions;
+        }
+
+        /*
+        *   Validates the choice
+        */
+
+
+        bool validate(int position, int val){
+            if(this->positions[position] == val){
+                return true;
+            }else{
+                return false;
+            }
+        }
+};
