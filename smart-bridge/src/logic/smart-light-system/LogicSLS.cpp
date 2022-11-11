c578248b408b99cab9198e5188aaadcd518576ca
Matteo <70414065+MatteoIorio11@users.noreply.github.com>
2022-11-11 10:15:05 +0000

    fixed the logic of smart light system

--- smart-bridge/src/logic/smart-light-system/LogicSLS.cpp
+++ smart-bridge/src/logic/smart-light-system/LogicSLS.cpp
@@ -1,12 +1,17 @@
 #include <TimerOne.h>
 #include <EnableInterrupt.h>
-#include "SmartLightSystem.h"
+#include "photoresistor/PhotoResistor.h"
+#include "led/GreenLed.h"
+#include "pir/Pir.h"
+GreenLed* la;
+PhotoResistor* ls;
+Pir* pir;
 
-#define DETECTED 1
-#define NOT_DETECTED 0
-
-SmartLightSystem* sls = new SmartLightSystem(1,2);
-
-void manageSmartLightSystem(){
+void init(int pin_pir, int pin_led, int pin_photo){
+    la = new GreenLed(pin_led);
+    pir = new Pir(pin_pir);
+    ls = new PhotoResistor(pin_photo);
 
 }
+
+
