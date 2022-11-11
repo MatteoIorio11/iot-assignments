d430b9412049c6eb1d2c1a363443cb4e241718c3
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-11-11 19:28:25 +0100

    MNoved folder

---
+++ smart-bridge/src/waterflowControlSystem/WaterflowControlSystem.cpp
@@ -0,0 +1,31 @@
+#include "WaterflowContolSystem.h"
+#include "sonar/Sonar.h"
+#include "Arduino.h"
+
+
+WaterflowControlSystem::WaterflowControlSystem(int echoPin, int trigPin){
+    this->echoPin = echoPin;
+    this->trigPin = trigPin;
+    this->WarningLevels = NORMAL;
+}
+
+void WaterflowControlSystem::init(){
+    this->sonar = new Sonar(this->echoPin, this->trigPin);
+}
+
+void WaterflowControlSystem::tick(){
+    this->detectWarningState();
+    switch (this->WarningLevels)
+    {
+    case NORMAL:
+        break;
+    case WL1:
+        break;
+    case WL2:
+        break;
+    }
+}
+
+void WaterflowControlSystem::detectWarningState(){
+    //this->WarningLevels = this->sonar->getDistance 
+}
