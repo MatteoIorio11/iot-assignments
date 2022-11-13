de0fcdea137d0c975278b0a1454a7242877ebbab
Fabio Vincenzi <fabio.vincenzi2@studio.unibo.it>
2022-11-13 15:51:11 +0100

    constructor of motor control

--- smart-bridge/src/functionalities/motor-control/MotorControl.cpp
+++ smart-bridge/src/functionalities/motor-control/MotorControl.cpp
@@ -4,3 +4,15 @@
 #include "hardware/servomotor/ServoMotor.h"
 #include "State.h"
 #include "Arduino.h"
+
+MotorControl::MotorControl(int pin_servo, int pin_pot, int pin_button){
+    this->pin_button = pin_button;
+    this->pin_pot = pin_pot;
+    this->pin_servo = pin_servo;
+}
+
+MotorControl::init(){
+    this->potentiometer = new Potentiometer(this->pin_pot);
+    this->button = new Button(this->pin_button);
+    this->servoMotor = new ServoMotor(this->pin_servo);
+}
