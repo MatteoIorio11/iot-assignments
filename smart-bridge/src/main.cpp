b1e5bda28087ff0fd2068190a7d660467531d8f7
Matteo <matteo.iorio01@gmail.com>
2022-11-14 16:30:06 +0100

    Added timer1 inside the main

--- smart-bridge/src/main.cpp
+++ smart-bridge/src/main.cpp
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+#include <TimerOne.h>
+
 #include "logic/logic-smart-light-system/LogicSLS.h"
 #include "logic/timer/ServoTimer2.h"
 #define PIN_LED 12
@@ -8,8 +10,15 @@
 
 ServoTimer2 timer;
 
+void function(){
+
+}
+
 void setup() {
   Serial.begin(9600);
+  Timer1.initialize();
+  Timer1.initialize(100000);
+  Timer1.attachInterrupt(function);
   initSLS(PIN_PIR, PIN_LED, PIN_PHOTORESISTOR, timer.read());
   /*
   if(PERIOD > 0){
