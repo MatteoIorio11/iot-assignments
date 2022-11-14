6705efe10f17b7fb5005fd0d1450a42306b4f6c4
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-11-14 23:05:58 +0100

    Implemented final state machine

--- smart-bridge/src/logic/logic-water-control-system/LogicWCS.cpp
+++ smart-bridge/src/logic/logic-water-control-system/LogicWCS.cpp
@@ -20,8 +20,9 @@ void buttonHandler(){
     }
 }
 
-void initWCS(int pin_servo, int pin_pot, int pin_button){
+void initWCS(int pin_servo, int pin_pot, int pin_button, int sonar_echoPin, int sonar_trigPin, int red_pin_led, int green_pin_led, int address, int rows, int cols){
     mc = new MotorControl(pin_servo, pin_pot, pin_button);
+    wcs = new WaterflowControlSystem(sonar_echoPin, sonar_trigPin, red_pin_led, green_pin_led, address, rows, cols);
     enableInterrupt(mc->getButton().getPin(), buttonHandler, RISING);
 }
 
@@ -33,10 +34,19 @@ void tickWCS(){
     switch (wcs->getState())
     {
         case NORMAL:
+            wcs->turnOnGreenLed();
+            wcs->turnOffRedLed();
+            wcs->turnOffDisplay();
+            mc->automatic();
             break;
         case PRE_ALARM:
+            wcs->turnOnDisplay();
+            wcs->turnOffGreenLed();
+            wcs->RedLedBlink();
+            wcs->displayPreAlarm(wcs->getWaterLevel());
             break;
         case ALARM:
+            wcs->turnOnDisplay();
             switch (mc->getState())
             {
                 case OFF:
@@ -50,6 +60,9 @@ void tickWCS(){
                     break;
             
             }
+            //wcs->displayAlarm(wcs->getWaterLevel(), ) Need potentiometer alpha
+            wcs->turnOffGreenLed();
+            wcs->turnOnRedLed();
             break;
     }
 }
--- smart-bridge/src/logic/logic-water-control-system/LogicWCS.h
+++ smart-bridge/src/logic/logic-water-control-system/LogicWCS.h
@@ -1,7 +1,7 @@
 #ifndef __LOGICWCS__
 #define __LOGICWCS__
 
-void initWCS(int pin_servo, int pin_pot, int pin_button);
+void initWCS(int pin_servo, int pin_pot, int pin_button, int sonar_echoPin, int sonar_trigPin, int red_pin_led, int green_pin_led);
 void tickWCS();
 void automatic();
 void buttonHandler();
