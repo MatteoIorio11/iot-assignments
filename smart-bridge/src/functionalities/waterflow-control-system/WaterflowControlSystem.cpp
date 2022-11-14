2fe7ee297eef9990be353cf65edb02f195c8d5c3
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-11-14 23:05:16 +0100

    Add lcd

--- smart-bridge/src/functionalities/waterflow-control-system/WaterflowControlSystem.cpp
+++ smart-bridge/src/functionalities/waterflow-control-system/WaterflowControlSystem.cpp
@@ -5,11 +5,14 @@
 #include "Arduino.h"
 
 
-WaterflowControlSystem::WaterflowControlSystem(int sonar_echoPin, int sonar_trigPin, int red_pin_led, int green_pin_led){
+WaterflowControlSystem::WaterflowControlSystem(int sonar_echoPin, int sonar_trigPin, int red_pin_led, int green_pin_led, int address, int rows, int cols){
     this->sonar_echoPin = sonar_echoPin;
     this->sonar_trigPin = sonar_trigPin;
     this->red_pin_led = red_pin_led;
     this->green_pin_led = green_pin_led;
+    this->address = address;
+    this->rows = rows;
+    this->cols = cols;
     this->state = NORMAL;
 }
 
@@ -17,6 +20,7 @@ void WaterflowControlSystem::init(){
     this->sonar = new Sonar(this->sonar_echoPin, this->sonar_trigPin);
     this->greenLed = new GreenLed(this->green_pin_led);
     this->redLed = new RedLed(this->red_pin_led); 
+    this->lcd = new MonitorLcd(address, rows, cols);
 }
 
 void WaterflowControlSystem::turnOnGreenLed(){
@@ -35,6 +39,10 @@ void WaterflowControlSystem::turnOffRedLed(){
     this->redLed->ledOff();
 }
 
+void WaterflowControlSystem::RedLedBlink(){
+    this->redLed->blink();
+}
+
 void WaterflowControlSystem::refreshWaterState(){
     if(this->getWaterLevel() > 2 && this->getWaterLevel() <= 150){
         this->state = NORMAL;
@@ -44,6 +52,22 @@ void WaterflowControlSystem::refreshWaterState(){
     }else if(this->getWaterLevel() > 300){
         this->state = ALARM;
     }
+}
+
+void WaterflowControlSystem::displayAlarm(double level, int op_degree){
+    this->lcd->displayAlarm(level, op_degree);
+}
+
+void WaterflowControlSystem::displayPreAlarm(double level){
+    this->lcd->displayPreAlarm(level);
+}
+
+void WaterflowControlSystem::turnOnDisplay(){
+    this->lcd->displayON();
+}
+
+void WaterflowControlSystem::turnOffDisplay(){
+    this->lcd->displayOFF();
 }
 
 double WaterflowControlSystem::getWaterLevel(){
