95cfa8af2ec9d2d41bb859d25b52304c0c33e7b9
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-11-15 15:24:04 +0100

    Add methods

--- smart-bridge/src/logic/logic-water-control-system/LogicWCS.cpp
+++ smart-bridge/src/logic/logic-water-control-system/LogicWCS.cpp
@@ -21,6 +21,20 @@ void buttonHandler(){
     }
 }
 
+void refreshWaterState(){
+    WaterState tmpState;
+    if(wcs->getWaterLevel() > MINIMUM_SONAR_DISTANCE && wcs->getWaterLevel() <= WL1_BOUND){
+        tmpState = NORMAL;
+    }else if(wcs->getWaterLevel() > WL1_BOUND && wcs->getWaterLevel() <= WL2_BOUND){
+        tmpState = PRE_ALARM;
+    }else if(wcs->getWaterLevel() > WL2_BOUND){
+        tmpState = ALARM;
+    }
+    if(wcs->getState() != tmpState){
+        wcs->updateState(tmpState);
+    }
+}
+
 void initWCS(int pin_servo, int pin_pot, int pin_button, int sonar_echoPin, int sonar_trigPin, int red_pin_led, int green_pin_led, int address, int rows, int cols){
     mc = new MotorControl(pin_servo, pin_pot, pin_button);
     wcs = new WaterflowControlSystem(sonar_echoPin, sonar_trigPin, red_pin_led, green_pin_led, address, rows, cols);
@@ -32,15 +46,25 @@ void automatic(){
 }
 
 void tickWCS(){
-    wcs->refreshWaterState();
+    refreshWaterState();
     switch (wcs->getState())
     {
         case NORMAL:
+<<<<<<< HEAD
             tickSLS();
             break;
         case PRE_ALARM:
             tickSLS();
+=======
+            wcs->turnOffDisplay();
+            wcs->turnOnGreenLed();
+            wcs->turnOffRedLed();
+            break;
+        case PRE_ALARM:
+            wcs->turnOffGreenLed();
+>>>>>>> b139c50 (Add methods)
             wcs->RedLedBlink();
+            wcs->turnOnDisplay();
             wcs->displayPreAlarm(wcs->getWaterLevel());
             break;
         case ALARM:
--- smart-bridge/src/logic/logic-water-control-system/LogicWCS.h
+++ smart-bridge/src/logic/logic-water-control-system/LogicWCS.h
@@ -1,9 +1,15 @@
 #ifndef __LOGICWCS__
 #define __LOGICWCS__
 
+#define MINIMUM_SONAR_DISTANCE 2
+#define WL1_BOUND 150
+#define WL2_BOUND 300
+#define MAXIMUM_SONAR_DISTANCE 400
+
 void initWCS(int pin_servo, int pin_pot, int pin_button, int sonar_echoPin, int sonar_trigPin, int red_pin_led, int green_pin_led);
 void tickWCS();
 void automatic();
 void buttonHandler();
+void refreshWaterState();
 
 #endif
