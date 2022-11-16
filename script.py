6989c593f5e1649226aeddf92c8a4fc933c32519
Matteo <matteo.iorio01@gmail.com>
2022-11-16 14:47:17 +0100

    added a function for auto-update a graph

--- script.py
+++ script.py
@@ -1,17 +1,27 @@
 import serial
 import json
 import time
+from matplotlib import animation
+import random as rnd
+import matplotlib.pyplot as plt
+
+#arduino = serial.Serial(port='/dev/ttyACM0', baudrate=9600)
+count = 0
+x=[]
+y=[]
+
+def myFunction(i):
+    global count
+    count +=1
+
+    x.append(count)
+    y.append(rnd.randint(1,10))
+    plt.cla()
+    plt.plot(x,y)
 
 
-arduino = serial.Serial(port='/dev/ttyACM0', baudrate=9600)
 
-def main():
-    while True:
-        if(arduino.inWaiting() > 0):
-            input = arduino.readline()
-            jsonFromatted = json.loads(input)
-            print(jsonFromatted)
 
-if __name__ == "__main__":
-    main()
+anima = animation.FuncAnimation(plt.gcf(), myFunction, interval=1500)
 
+plt.show()
