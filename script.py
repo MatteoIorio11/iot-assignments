aa29bbcb030726546898aacab544e96102e88479
Matteo <matteo.iorio01@gmail.com>
2022-11-16 14:20:58 +0100

    load of the received json inside the scrypt.py

--- script.py
+++ script.py
@@ -7,7 +7,10 @@ arduino = serial.Serial(port='/dev/ttyACM0', baudrate=9600)
 
 def main():
     while True:
-	    print(str(arduino.readline()))
+        if(arduino.inWaiting() > 0):
+            input = arduino.readline()
+            jsonFromatted = json.loads(input)
+            print(jsonFromatted)
 
 if __name__ == "__main__":
     main()
