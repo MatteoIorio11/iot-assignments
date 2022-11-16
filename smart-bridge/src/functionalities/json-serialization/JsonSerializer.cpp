e5219f7c9158e2da73d64cf23e72829b3914cee5
Fabio Vincenzi <fabio.vincenzi2@studio.unibo.it>
2022-11-16 09:29:50 +0100

    starting implementing serializeAlarm

--- smart-bridge/src/functionalities/json-serialization/JsonSerializer.cpp
+++ smart-bridge/src/functionalities/json-serialization/JsonSerializer.cpp
@@ -2,7 +2,15 @@
 
 void JsonSerializer::serializePreAlarm(double waterLevel){
     DynamicJsonDocument doc(1024);
-    doc["sewaterLevel"] = waterLevel;
+    doc["waterLevel"] = waterLevel;
+
+    serializeJson(doc, Serial);
+}
+
+void JsonSerializer::serializeAlarm(double waterLevel, int angle){
+    DynamicJsonDocument doc(1024);
+    doc["waterLevel"] = waterLevel;
+    doc["angle"] = angle;
 
     serializeJson(doc, Serial);
 }
