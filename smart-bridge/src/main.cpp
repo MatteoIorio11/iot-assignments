2c7381b8dc5021aeff9548cb1672cc7e8e770641
Matteo <matteo.iorio01@gmail.com>
2022-11-19 15:06:33 +0100

    testing the json

--- smart-bridge/src/main.cpp
+++ smart-bridge/src/main.cpp
@@ -3,7 +3,10 @@
 #include <ArduinoJson.h>
 #include "bridge/Bridge.h"
 #include "functionalities/json/json-serialization/JsonSerializer.h"
+#include "hardware/servomotor/ServoMotor.h"
 Bridge* b;
+ServoMotor *s;
+
 void changeTheState(){
   b->getTimer()->changeState();
 }
@@ -11,14 +14,26 @@ void changeTheState(){
 void setup()
 {
   Serial.begin(9600);
-  b = new Bridge();
-  b->init();
-  b->getTimer()->getTimerOne()->attachInterrupt(changeTheState); 
+  //s = new ServoMotor(A1);
+  //b = new Bridge();
+  //b->init();
+  //b->getTimer()->getTimerOne()->attachInterrupt(changeTheState); 
 }
 
 
 void loop() {
+  /*
+  for(int i = 0; i < 180; i++){
+    s->setAngle(i);
+    delay(10);
+  }
+  delay(2000);
+  s->setAngle(0);
+  delay(2000);
+  */
   // put your main code here, to run repeatedly:
-  b->getTimer()->waitForTheNextTick();
-  b->tick();
+  //b->getTimer()->waitForTheNextTick();
+  //b->tick();
+  JsonSerializer::serialize(NORMAL, 10.0, 1);
+  delay(1000);
 }
