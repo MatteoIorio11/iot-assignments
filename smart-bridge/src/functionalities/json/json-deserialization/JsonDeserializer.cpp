c8a86a541345fd840298feacdbe42a4a6665f4ec
Fabio Vincenzi <fabio.vincenzi2@studio.unibo.it>
2022-11-29 14:31:35 +0100

    addng previous version of deserialization

--- smart-bridge/src/functionalities/json/json-deserialization/JsonDeserializer.cpp
+++ smart-bridge/src/functionalities/json/json-deserialization/JsonDeserializer.cpp
@@ -6,6 +6,31 @@ this could be extended with more parameters in the future
     "angle": x
 }
 */
+
+/*
+__Old version for the serialization__
+
+int JsonDeserializer::getAngle(){
+    String json = Serial.readString();
+
+    if(!json.equals("")){
+        int index = json.indexOf(':');
+        String angle_json = json.substring(index+2, json.length()-1);
+        if(angle_json.equals("")){
+            return CODE_EMPTY_MESSAGE;
+        }else{
+            int angle =  angle_json.toInt(); 
+            if(angle >= 0){
+                return angle;
+            }else{
+                return CODE_SWITCH_CONTROL;
+            }
+        }
+    }else{
+        return CODE_EMPTY_MESSAGE;
+    }
+}
+*/
 int JsonDeserializer::getAngle(){
     String json = Serial.readString(); //reading the JSON from the serial
 
