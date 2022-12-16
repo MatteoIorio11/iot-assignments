13849ff2e2a2bcd63167c52dbf4af1c26bde9ae1
Fabio Vincenzi <fabio.vincenzi2@studio.unibo.it>
2022-12-16 10:11:53 +0100

    creating the slider and the witch button and the events listener

--- room-app/app/src/main/java/com/example/room_app/MainActivity.java
+++ room-app/app/src/main/java/com/example/room_app/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.room_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Switch ledSwitch;
+    private SeekBar servoBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.ledSwitch = findViewById(R.id.ledSwitch);
+        this.servoBar = findViewById(R.id.servoBar);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        this.ledSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // do something, the isChecked will be
+                // true if the switch is in the On position
+            }
+        });
+        this.servoBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,boolean fromUser) {
+                // TODO Auto-generated method stub
+            }
+        });
     }
 }
