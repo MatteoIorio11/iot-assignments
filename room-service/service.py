9e860ca8ee925b45f160e06f62ccec4f6940735a
Matteo <matteo.iorio01@gmail.com>
2022-12-18 12:32:43 +0100

    added locks inside the code

--- room-service/service.py
+++ room-service/service.py
@@ -3,11 +3,14 @@ from flask import Flask
 from flask import request
 from paho.mqtt import client as mqtt_client
 from threading import Thread
+import threading
 import json
 from datetime import datetime
 import time 
 import serial
 
+# ==================== LOCK FOR THREADING
+lock = threading.Lock()
 # ==================== SETUP THE SERVER
 app = Flask(__name__)
 # ==================== SETUP ARDUINO
@@ -63,21 +66,29 @@ def subscribe(client: mqtt_client, total_t: int, start_t: int, res: list, w:list
 def calcTime(json_message: dict, total_t: int, start_t: int, res: list, w:list):
     total_time = total_t
     start_time = start_t
+    lock.acquire()
     w[2] = True
+    lock.release()
     if json_message["inside_room"] == False:
-            components[2] = False
+        lock.acquire()
+        components[2] = False
+        lock.release()
+        if(res[0] >= 0):
             total_time += get_sec(json_message["time"]) - res[0]
             res[1] += total_time
     else:
+        lock.acquire()
         components[2] = True
+        lock.release()
         if json_message["state"] == "ON":
             #get the start's seconds of the LED ON 
             start_time = get_sec(json_message["time"])
             res[0] = start_time
         else:
             if(res[0] > 0):
-                total_time += get_sec(json_message["time"]) - res[0]
-                res[1] += total_time
+                if(res[0] >= 0):
+                    total_time += get_sec(json_message["time"]) - res[0]
+                    res[1] += total_time
     
 
 ### CONVERT HH:MM:SS into seconds
@@ -102,8 +113,10 @@ def handlerServo():
     if request.method == 'GET':
         angle = request.args.get('angle')
         if angle != None:
+            lock.acquire()
             components[0] = angle
             writes[0] = True # Arduino has to write
+            lock.release()
 
         # Send the message to the Client
         j = json.dumps({'angle': components[0]})
@@ -117,8 +130,10 @@ def handlerLed():
     if request.method == 'GET':
         state = request.args.get('state')
         if state != None:
+            lock.acquire()
             components[1] = False if state == "OFF" else True
             writes[1] = True # Arduino has to write
+            lock.release()
         
         # Send the message to the Client
         j = json.dumps({'state': "ON" if components[1] == True else "OFF"})
@@ -137,55 +152,77 @@ def startClient():
 def startServer():
     app.run(debug=False, host='127.0.0.100',port=5000)
 
-def startArduino():
-    
-    curr_hour, curr_min, curr_sec  = str(datetime.now().time()).split(':')
+def startArduino():    
     while True:
+        curr_hour, curr_min, curr_sec  = str(datetime.now().time()).split(':')
         if(int(curr_hour) == 8 and int(curr_min) == 0 and int(curr_sec) == 0):
             j = json.dumps({'hardware': TIME_TAG, 'time': "8", 'state':components[1], 'angle':components[0], 'inside_room':components[2]})
             print(j)
         elif (int(curr_hour) == 19 and int(curr_min) == 0 and int(curr_sec) == 0):
             j = json.dumps({'hardware':TIME_TAG, 'time': "19", 'state':components[1], 'angle':components[0], 'inside_room':components[2]})
-        #if(arduino.inWaiting() > 0):
+        if(1 > 0):
+            try:
+                #msg = arduino.readLine()
+                msg_p = j.loads(msg)
+                tmp = list()
+                tmp.append(msg_p['state'])
+                tmp.append(msg_p['angle'])
+            
+                lock.acquire()
+                components[0] = tmp[0]
+                components[1] = tmp[1]
+                lock.release()
+            except Exception as e:
+                time.sleep(0.5)
             #read the message
             #msg = arduino.readline()
             #msg_dese = json.loads(msg)
             #save the infos about Servo and Led inside a variable and the there will be a button inside the Dashboard
             #such as Refresh, and It will return the infos about
         if (writes[1]):
+            lock.acquire()
             j = json.dumps({'hardware':LED_TAG, 'time': curr_hour, 'state':components[1], 'angle':components[0], 'inside_room':components[2]})
-            print("Send data to arduino")
             #invia un msg contenente le info del led
             writes[1] = False
+            lock.release()
             #arduino.write(bytes(j, 'utf-8'))
         if (writes[0]):
+            lock.acquire()
             j = json.dumps({'hardware':SERVO_TAG, 'time': curr_hour, 'state':components[1], 'angle':components[0], 'inside_room':components[2]})
-            print(j)
             #invia un msg contentente le info del servo
             writes[0] = False
+            lock.release()
             #arduino.write(bytes(j, 'utf-8'))
         if (writes[2]):
+            lock.acquire()
             j = json.dumps({'hardware':PIR_TAG, 'time': curr_hour, 'state':components[1], 'angle':components[0], 'inside_room':components[2]})
             writes[2] = False
+            lock.release()
+            #arduino.write(bytes(j, 'utf-8'))
             print(j)
         
         time.sleep(2)
         
-        
 
-def run():
+def initVariables():
+    # Variables used inside the calc of the total seconds 
+    times.append(-1)
     times.append(0)
-    times.append(0)
+    # Variables used for communicate with Arduino
     writes.append(False) # Write Servo's information
     writes.append(False) # Write Led's information
     writes.append(False) # Write PIR's information
+    # Components of our system
     components.append(0)
     components.append(False)
     components.append(False)
-    
+
+def run():
+
+    initVariables()    
     mqtt_thread = Thread(target=startClient)
     server_thread = Thread(target=startServer)
-    #arduino_thread = Thread(target=startArduino)
+    arduino_thread = Thread(target=startArduino)
     mqtt_thread.start()
     server_thread.start()
     arduino_thread.start()
